9cd90fcd3722f597494c8440a423fd4bec799e0f
Alfredo Fernandez <alfredofernandeza@gmail.com>
2011-09-23 21:47:52 +0100

    added override where necessary and removed parens from method declarations

--- src/com/mark/adt/GPAMark.scala
+++ src/com/mark/adt/GPAMark.scala
@@ -1,16 +1,15 @@
 package com.mark.adt
 
-/** 
- * Represents a simple grading scheme given by value/scale. 
- * 
- * A grade of 8/10 is thus constructed by specifying a score of 8 in a 
- * scale of 10. Any arbitrary (positive) scale is allowed. 
- * 
+/**
+ * Represents a simple grading scheme given by value/scale.
+ *
+ * A grade of 8/10 is thus constructed by specifying a score of 8 in a
+ * scale of 10. Any arbitrary (positive) scale is allowed.
+ *
  * @param score The actual score represented by this mark.
  * @param scale The maximum score attainable in this assignment.
  */
-class GPAMark(score: Int, scale : Int) extends Mark {
-	def intValue() = this.score;
-	def stringValue() = this.score + "/" + this.scale;
-  
+class GPAMark(score: Int, scale: Int) extends Mark {
+  override def intValue = score;
+  override def stringValue = score + "/" + scale;
 }
