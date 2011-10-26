2bc92594758d0185d73699762f69a5ed1abdd363
Julián Hernández Muñoz <julianhernandezmunoz@gmail.com>
2011-10-26 00:17:55 -0700

    Create skeleton for ExcelDataSource. AddApache POI jars.

--- src/com/mark/data/DataSourceFactory.scala
+++ src/com/mark/data/DataSourceFactory.scala
@@ -9,8 +9,17 @@ object DataSourceFactory {
   /**
    * Create data source from CSV file
    */
-  def loadCSV(descriptor: CSVDataDescriptor): CSVDataSource = {
+  def loadCSV(descriptor: CSVDataDescriptor): GradeDataSource = {
     val dataSource = new CSVDataSource
+    dataSource.loadData(descriptor)
+    return dataSource
+  }
+
+  /**
+   * Create data source from Microsoft Excel file
+   */
+  def loadExcel(descriptor: ExcelDataDescriptor): GradeDataSource = {
+    val dataSource = new ExcelDataSource
     dataSource.loadData(descriptor)
     return dataSource
   }
---
+++ src/com/mark/data/DataSourceUtil.scala
@@ -0,0 +1,13 @@
+package com.mark.data
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: julian
+ * Date: 10/25/11
+ * Time: 11:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object DataSourceUtil {
+
+}
